dd209614964eeb7ac5f967147e128150d1ab9793
Vrushali <118001762+vrushali-devlekar@users.noreply.github.com>
2025-07-30 10:11:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var rect = document.querySelector("#rectangle");
+
+rect.addEventListener("mousemove", function (dets) {
+  var rectLocation = rect.getBoundingClientRect();
+  var insideRect = dets.clientX - rectLocation.left;
+
+  if (insideRect < rectLocation.width / 2) {
+    var redColor = gsap.utils.mapRange(
+      0,
+      rectLocation.width / 2,
+      255,
+      0,
+      insideRect
+    );
+    gsap.to(rect, {
+      backgroundColor: `rgb(${redColor},0,0)`,
+      ease: Power4,
+    });
+  } else {
+    var BlueColor = gsap.utils.mapRange(
+      rectLocation.width / 2,
+      rectLocation.width,
+      0,
+      255,
+      insideRect
+    );
+    gsap.to(rect, {
+      backgroundColor: `rgb(0,0,${BlueColor})`,
+      ease: Power4,
+    });
+  }
+});
